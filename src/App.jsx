a41e7c155481a974e6f9cd17bd918f9b2a7134e9
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-13 16:11:39 +0100

    Create layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AppLayout from './AppLayout';
+import Home from './pages/Home';
+
 export default function App() {
 	return (
-		<div>
-			TEST
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<AppLayout />}>
+					<Route path='/' element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	)
 }
