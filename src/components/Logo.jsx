f1c7e8502cc4f183b4dcb083e18bfbaeba338270
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-14 17:24:43 +0100

    Style navigation bar

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { IoHome } from "react-icons/io5";
+import './logo.scss';
 
 export default function Logo() {
     return (
         <div className='logo'>
             <Link className='logo__link' to={'/'}>
-                LOGO
+                <IoHome size={'1.625rem'}/>
             </Link>
         </div>
     )
