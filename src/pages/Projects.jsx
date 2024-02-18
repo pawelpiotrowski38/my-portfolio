70c9a201d3f864a88116dc878b6d246a8ce64458
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-02-18 19:36:15 +0100

    Create projects section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,13 @@
+import ProjectsList from "../components/ProjectsList";
+import './projects.scss';
+
 export default function Projects() {
     return (
-        <div>
-            PROJECTS
-        </div>
+        <section className='projects'>
+            <h2 className='projects__header'>
+                My projects
+            </h2>
+            <ProjectsList />
+        </section>
     )
 }
